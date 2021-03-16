d6844e93b6af85e8460e685524cbf3dd22bfc5f3
FabiOquendo <fabioquendo95@gmail.com>
2021-03-16 09:13:53 -0500

    4A-Watermelon solution

---
+++ 4A-Watermelon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace codeforces_solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            int n = int.Parse(line);
+            if (n != 2 && n % 2 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ codeforces-solutions/codeforces-solutions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace codeforces_solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 4A-Watermelon
+
+            string line = Console.ReadLine();
+            int n = int.Parse(line);
+            if (n != 2 && n % 2 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
